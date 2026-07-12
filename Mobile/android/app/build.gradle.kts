4adfb612692d59fe30dfa333182abf45fa46e2b4
kaushalsatani <aicluster.ai@gmail.com>
2026-07-12 09:29:36 +0530

    firebase files added to project and bundle ids updated.

--- Mobile/android/app/build.gradle.kts
+++ Mobile/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.transit.op.app.transit_op"
+    namespace = "com.transit.ops.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.transit.op.app.transit_op"
+        applicationId = "com.transit.ops.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
